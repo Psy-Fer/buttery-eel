920a940c4c06ab204efe79f110d39f47cd34c859
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2023-10-03 11:53:22 +1100

    Update install-eel.sh

--- test/install-eel.sh
+++ test/install-eel.sh
@@ -12,7 +12,7 @@ git clone https://github.com/Psy-Fer/buttery-eel -b main || die "git clone faile
 mv buttery-eel buttery-eel-0.3.1+${GUPPY_VERSION} || die "mv failed"
 cd buttery-eel-0.3.1+${GUPPY_VERSION} || die "cd failed"
 #git checkout 855fff2 || die "git checkout failed"
-sed -i "s/6.3.8/${GUPPY_VERSION}/" requirements.txt || die "sed failed"
+sed -i "s/6.5.7/${GUPPY_VERSION}/" requirements.txt || die "sed failed"
 
 python3 -m venv venv3 || die "venv failed"
 source venv3/bin/activate || die "venv activate failed"

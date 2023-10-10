9f92474d1902f068918d427340d238052fd9d7a0
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2023-10-10 23:18:00 +1100

    restructure scripts

--- test/test_extensive.sh
+++ test/test_extensive.sh
@@ -4,8 +4,8 @@ set -e
 
 rm -f *.log
 rm -rf logs_guppy logs_dorado
-test/guppy/test_extensive.sh 
-mkdir logs_guppy 
+test/guppy/test_extensive.sh
+mkdir logs_guppy logs_dorado
 mv *.log logs_guppy
 test/dorado/test_install.sh
 mv *.log logs_dorado

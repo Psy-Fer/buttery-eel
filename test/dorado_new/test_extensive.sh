965db2898bea024ba4ec8154a72bf3ea2d1088eb
Psy-Fer <fergo1988@gmail.com>
2025-12-08 15:39:24 +1100

    comment tests for testing

--- test/dorado_new/test_extensive.sh
+++ test/dorado_new/test_extensive.sh
@@ -60,7 +60,7 @@ echo "R10.4.1 DNA - FAST model - 20k reads - resume"
 export PATH_TO_FAST5=/data/slow5-testdata/hg2_prom_lsk114_5khz_subsubsample/pod5/
 export PATH_TO_BLOW5=/data/slow5-testdata/hg2_prom_lsk114_5khz_subsubsample/reads.blow5
 export MODEL=dna_r10.4.1_e8.2_400bps_fast@v5.2.0
-test/dorado_new/test_resume.sh &> r10_resume.log || die "test failed. see r10_resume.log for details"
+#test/dorado_new/test_resume.sh &> r10_resume.log || die "test failed. see r10_resume.log for details"
 echo ""
 echo "********************************************************************"
 
@@ -68,7 +68,7 @@ echo "R10.4.1 DNA - HAC model - 20k reads - split qscore inbuilt"
 export PATH_TO_FAST5=/data/slow5-testdata/hg2_prom_lsk114_5khz_subsubsample/pod5/
 export PATH_TO_BLOW5=/data/slow5-testdata/hg2_prom_lsk114_5khz_subsubsample/reads.blow5
 export MODEL=dna_r10.4.1_e8.2_400bps_fast@v5.2.0
-test/dorado_new/test_qscore_split.sh &> r10_split1.log || die "test failed. see r10_split1.log for details"
+#test/dorado_new/test_qscore_split.sh &> r10_split1.log || die "test failed. see r10_split1.log for details"
 echo ""
 echo "********************************************************************"
 
@@ -76,7 +76,7 @@ echo "R10.4.1 DNA - FAST model - 20k reads - split qscore script"
 export PATH_TO_FAST5=/data/slow5-testdata/hg2_prom_lsk114_5khz_subsubsample/pod5/
 export PATH_TO_BLOW5=/data/slow5-testdata/hg2_prom_lsk114_5khz_subsubsample/reads.blow5
 export MODEL=dna_r10.4.1_e8.2_400bps_fast@v5.2.0
-test/dorado_new/test_qscore_split2.sh &> r10_split2.log || die "test failed. See r10_split2.log for details"
+#test/dorado_new/test_qscore_split2.sh &> r10_split2.log || die "test failed. See r10_split2.log for details"
 echo ""
 echo "********************************************************************"
 
@@ -90,7 +90,7 @@ export PATH_TO_FAST5=/data/slow5-testdata/hg2_prom_lsk114_5khz_subsubsample/pod5
 export PATH_TO_BLOW5=/data/slow5-testdata/hg2_prom_lsk114_5khz_subsubsample/reads.blow5
 export OPTS_GUPPY="--trim_adapters"
 export OPTS_EEL="--trim_adapters"
-test/dorado_new/test.sh &> r10_adaptertrim.log  || echo "test failed. See r10_adaptertrim.log for details"
+#test/dorado_new/test.sh &> r10_adaptertrim.log  || echo "test failed. See r10_adaptertrim.log for details"
 unset OPTS_GUPPY
 unset OPTS_EEL
 echo ""
@@ -121,14 +121,14 @@ echo "********************************************************************"
 echo "seqsum"
 export PATH_TO_FAST5=/data/slow5-testdata/hg2_prom_lsk114_5khz_subsubsample/pod5/
 export PATH_TO_BLOW5=/data/slow5-testdata/hg2_prom_lsk114_5khz_subsubsample/reads.blow5
-test/dorado_new/test_seqsum.sh &> seqsum.log || die "test failed. See seqsum.log for details"
+#test/dorado_new/test_seqsum.sh &> seqsum.log || die "test failed. See seqsum.log for details"
 echo ""
 echo "********************************************************************"
 
 echo "seqsum - multiple BLOW5"
 export PATH_TO_FAST5=/data/slow5-testdata/hg2_prom_lsk114_5khz_subsubsample/pod5/
 export PATH_TO_BLOW5=/data/slow5-testdata/hg2_prom_lsk114_5khz_subsubsample/reads.blow5
-test/dorado_new/test_seqsum.sh &> seqsum_multiblow.log
+#test/dorado_new/test_seqsum.sh &> seqsum_multiblow.log
 echo ""
 echo "********************************************************************"
 
